c548594965e8aa1b2582bfd17e3d9472fcc66bcc
thangvd2312 <thangvd2312@gmail.com>
2023-04-18 15:19:33 +0700

    [Add] Tìm hiểu Refs, Reactive Objects

--- proj-vue/src/views/HomeView.vue
+++ proj-vue/src/views/HomeView.vue
@@ -1,9 +1,88 @@
+<template>
+  <div class="home">
+    <h3>{{ counterData.title }}</h3>
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input type="text" v-model="counterData.title">
+    </div>
+
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { reactive, ref } from 'vue'
+const counter = ref(0)
+const counterTitle = ref('My Counter');
+const counterData = reactive({
+  count: 10,
+  title: 'My Counter',
+})
+const increaseCounter = () => {
+  counterData.count++;
+}
+const decreaseCounter = () => {
+  counterData.count--;
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<!--
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const counter = ref(0)
+    const increaseCounter = () => {
+      counter.value++
+    }
+    const decreaseCounter = () => {
+      counter.value--
+    }
+    return {
+      counter,
+      increaseCounter,
+      decreaseCounter
+    }
+  }
+}
+</script>
+-->
+
+<!--
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++
+    },
+    decreaseCounter() {
+      this.counter--
+    }
+  }
+}
+</script>
+-->
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+.edit {
+  margin-top: 60px;
+}
+</style>
