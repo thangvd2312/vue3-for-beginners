bad9be0b36430e609470898ba5b4a356b461ffcf
thangvd2312 <thangvd2312@gmail.com>
2023-05-29 17:42:48 +0700

    [Update] Completed game pokemon use vue js

--- pokemon-memory/src/App.vue
+++ pokemon-memory/src/App.vue
@@ -3,20 +3,37 @@
     v-if="statusMatch === 'default'"
     @onStart="onHandleBeforeStart($event)"
   />
-  <interact-component v-if="statusMatch === 'interact'" />
-  <footer-component />
+  <interact-component
+    v-if="statusMatch === 'interact'"
+    :cardsContext="settings.cardsContext"
+    @onFinish="onGetResult"
+  />
+  <result-component
+    v-if="statusMatch === 'result'"
+    :timer="timer"
+    @onStartAgain="statusMatch = 'default'"
+  />
+  <p class="copyright">
+    Mini Project created by thangvd2312 - Vue
+    <a
+      href="https://github.com/thangvd2312/vue3-for-beginners/tree/main/pokemon-memory"
+      >View here</a
+    >
+  </p>
 </template>
 
 <script>
-import FooterComponent from "./components/Footer.vue";
 import MainComponent from "./components/Main.vue";
 import InteractComponent from "./components/Interact.vue";
+import ResultComponent from "./components/Result.vue";
+import { shuffled } from "./utils/array";
+
 export default {
   name: "App",
   components: {
     MainComponent,
-    FooterComponent,
     InteractComponent,
+    ResultComponent,
   },
   data() {
     return {
@@ -29,7 +46,41 @@ export default {
       statusMatch: "default",
     };
   },
+  methods: {
+    onHandleBeforeStart(configs) {
+      this.settings.totalOfBlocks = configs.totalOfBlocks;
+
+      const firstCards = Array.from(
+        { length: this.settings.totalOfBlocks / 2 },
+        (_, i) => i + 1
+      );
+      const secondCards = [...firstCards];
+      const cards = [...firstCards, ...secondCards];
+
+      this.settings.cardsContext = shuffled(shuffled(shuffled(cards)));
+      this.settings.startedAt = new Date().getTime();
+      this.statusMatch = "interact";
+    },
+    onGetResult() {
+      this.statusMatch = "result";
+      this.timer = new Date().getTime() - this.settings.startedAt;
+    },
+  },
 };
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.copyright {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 0;
+  color: var(--light);
+  z-index: 3;
+  font-size: 1.5rem;
+}
+
+.copyright a {
+  color: #f4dc26;
+}
+</style>
