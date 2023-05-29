f3c13e793223362c5e170d3d4efb3b33a90dcf02
thangvd2312 <thangvd2312@gmail.com>
2023-05-29 14:29:03 +0700

    [Update] Update component main

--- pokemon-memory/src/App.vue
+++ pokemon-memory/src/App.vue
@@ -1,7 +1,35 @@
 <template>
-  <h1>App Project Mini</h1>
+  <main-component
+    v-if="statusMatch === 'default'"
+    @onStart="onHandleBeforeStart($event)"
+  />
+  <interact-component v-if="statusMatch === 'interact'" />
+  <footer-component />
 </template>
 
-<script></script>
+<script>
+import FooterComponent from "./components/Footer.vue";
+import MainComponent from "./components/Main.vue";
+import InteractComponent from "./components/Interact.vue";
+export default {
+  name: "App",
+  components: {
+    MainComponent,
+    FooterComponent,
+    InteractComponent,
+  },
+  data() {
+    return {
+      settings: {
+        totalOfBlocks: 0,
+        cardsContext: [],
+        startedAt: null,
+      },
+      timer: 0,
+      statusMatch: "default",
+    };
+  },
+};
+</script>
 
 <style lang="css" scoped></style>
---
+++ pokemon-memory/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="copyright">
+    Mini Project created by thangvd2312 - Vue
+    <a href="https://github.com/thangvd2312/vue3-for-beginners/tree/main/pokemon-memory">View here</a>
+  </p>
+</template>
+<style lang="css" scoped>
+.copyright {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 1.5rem;
+  color: var(--light);
+  z-index: 3;
+  font-size: 1.5rem;
+}
+
+.copyright a {
+  color: #f4dc26;
+}
+</style>
