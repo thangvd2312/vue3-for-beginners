211805ef7437ab137e9454209786df9cbb55d8c2
thangvd2312 <thangvd2312@gmail.com>
2023-05-29 13:36:43 +0700

    [Create] Project mini vue 3

---
+++ pokemon-memory/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <main-screen
+    v-if="statusMatch === 'default'"
+    @onStart="onHandleBeforeStart($event)"
+  />
+  <interact-screen
+    v-if="statusMatch === 'match'"
+    :cardsContext="settings.cardsContext"
+    @onFinish="onGetResult"
+  />
+  <result-screen
+    v-if="statusMatch === 'result'"
+    :timer="timer"
+    @onStartAgain="statusMatch = 'default'"
+  />
+  <p class="copyright">
+    This game owned by RHP Team in Vue 3 course for begginers -
+    <a
+      href="https://www.youtube.com/watch?v=CHM75-NqOmk&list=PLU4OBh9yHE94sZ3TPGt0QG_PIwrZ1QF6i"
+      >view here</a
+    >
+  </p>
+</template>
+
+<script>
+import MainScreen from "./components/MainScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+import ResultScreen from "./components/ResultScreen.vue";
+
+import { shuffled } from "./utils/array";
+
+export default {
+  name: "App",
+  components: {
+    MainScreen,
+    InteractScreen,
+    ResultScreen,
+  },
+  data() {
+    return {
+      settings: {
+        totalOfBlocks: 0,
+        cardsContext: [],
+        startedAt: null,
+      },
+      timer: 0,
+      statusMatch: "default",
+    };
+  },
+  methods: {
+    onHandleBeforeStart(configs) {
+      this.settings.totalOfBlocks = configs.totalOfBlocks;
+
+      const firstCards = Array.from(
+        { length: this.settings.totalOfBlocks / 2 },
+        (_, i) => i + 1
+      );
+      const secondCards = [...firstCards];
+      const cards = [...firstCards, ...secondCards];
+
+      this.settings.cardsContext = shuffled(shuffled(shuffled(cards)));
+      this.settings.startedAt = new Date().getTime();
+
+      this.statusMatch = "match";
+    },
+
+    onGetResult() {
+      this.statusMatch = "result";
+      this.timer = new Date().getTime() - this.settings.startedAt;
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.copyright {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 1.5rem;
+  color: var(--light);
+  z-index: 3;
+  font-size: 1.5rem;
+}
+
+.copyright a {
+  color: #f4dc26;
+}
+</style>
---
+++ pokemon-memory/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>App Project Mini</h1>
+</template>
+
+<script></script>
+
+<style lang="css" scoped></style>
